d2a56753050f8459e2b468712fb87114c2635c9b
one <wangan.cs@gmail.com>
2023-11-19 19:56:35 +0800

    Update esbuild.config.mjs

--- scripts/esbuild.config.mjs
+++ scripts/esbuild.config.mjs
@@ -95,15 +95,15 @@ esbuild
 								try {
 									copyFileSync(
 										"styles.css",
-										"../../vault/.obsidian/plugins/cmdr/styles.css"
+										"../../vault/.obsidian/plugins/obsidian-slash-commander/styles.css"
 									);
 									copyFileSync(
 										"main.js",
-										"../../vault/.obsidian/plugins/cmdr/main.js"
+										"../../vault/.obsidian/plugins/obsidian-slash-commander/main.js"
 									);
 									copyFileSync(
 										"manifest.json",
-										"../../vault/.obsidian/plugins/cmdr/manifest.json"
+										"../../vault/.obsidian/plugins/obsidian-slash-commander/manifest.json"
 									);
 								} catch (error) {
 									console.error(error);
