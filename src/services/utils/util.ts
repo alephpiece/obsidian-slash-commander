e1241a4b3e65d8ccb42f56a0ddc900ddb2cad119
one <wangan.cs@gmail.com>
2025-03-14 22:25:23 +0800

    refactor: remove useless modals

--- src/services/utils/util.ts
+++ src/services/utils/util.ts
@@ -1,35 +1,6 @@
-import { Command } from "obsidian";
 import { SlashCommand } from "@/data/models/SlashCommand";
-import AddCommandModal from "@/ui/modals/addCommandModal";
-import ChooseIconModal from "@/ui/modals/chooseIconModal";
-import ChooseCustomNameModal from "@/ui/modals/chooseCustomNameModal";
 import SlashCommanderPlugin from "@/main";
 import { t } from "i18next";
-
-/**
- * Creates a new command by prompting the user to select a command, icon, and name
- * @param {SlashCommanderPlugin} plugin - The plugin instance
- * @returns {SlashCommand} A new slash command object
- */
-export async function chooseNewCommand(plugin: SlashCommanderPlugin): Promise<SlashCommand> {
-	const command = await new AddCommandModal(plugin).awaitSelection();
-
-	let icon;
-	if (!command.hasOwnProperty("common.icon")) {
-		icon = await new ChooseIconModal(plugin).awaitSelection();
-	}
-
-	const name = await new ChooseCustomNameModal(plugin, command.name).awaitSelection();
-
-	return {
-		id: command.id,
-		// This cannot be undefined anymore
-		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-		icon: icon ?? command.icon!,
-		name: name || command.name,
-		mode: "any",
-	};
-}
 
 export function isTriggerInConflicts(plugin: SlashCommanderPlugin): boolean {
 	const settings = plugin.settingsStore.getSettings();
