c3ac188752a2e0d029bd03a1fc3b857621cd34b4
laktiv <laktionov.ivan@gmail.com>
2025-05-24 00:13:16 +0300

    feat(i18n): add Ukrainian language support

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -5,12 +5,14 @@ import { initReactI18next } from "react-i18next";
 import en from "@/i18n/resources/en.json";
 import ja from "@/i18n/resources/ja.json";
 import ru from "@/i18n/resources/ru.json";
+import uk from "@/i18n/resources/uk.json";
 import zhCN from "@/i18n/resources/zh-cn.json";
 
 const resources = {
     en,
     ja,
     ru,
+    uk,
     "zh-CN": zhCN,
 };
 
